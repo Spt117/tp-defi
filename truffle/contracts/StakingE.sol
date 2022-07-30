6e5937864ba88085b79082a5bf6355b927d00d5a
Etienne <etienne.herencia@gmail.com>
2022-07-30 11:41:07 +0200

    Add struct pool, staker and update stake and whitdraw

--- truffle/contracts/StakingE.sol
+++ truffle/contracts/StakingE.sol
@@ -2,16 +2,32 @@
 pragma solidity 0.8.14;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract StakingE {
+contract StakingE is AggregatorV3Interface {
 
 	IERC20 public stakingToken;
-
-	// How much they staked
-	mapping(address => uint256) public balances;
 	
 	uint256 private totalStake;
-	uint256 public rewardPerTokenStake;
+
+	struct Pool {
+		address token;
+		uint256 yield;
+		AggregatorV3Interface priceFeed;
+	}
+	Pool[] public pools;
+
+	struct Staker {
+		address addrStaker; // Address of wallet want stake
+		address token; // Address of token stake
+		uint256 amount; // Amount token stake
+		uint256 date; // Date of stake start
+	}
+	Staker[] public stakers;
+
+	// Save total amount stake by token address
+	struct TotalStake { uint256 amount; }
+	mapping (address => TotalStake) totalStakes;
 
 	constructor (address _stakingToken) {
 		stakingToken = IERC20(_stakingToken);
@@ -21,23 +37,31 @@ contract StakingE {
 	 * @notice Stake fund into this contract
 	 * @param _amount to stake
 	 */
-	function stake (uint256 _amount) external {
+	function stake (uint256 _amount, address _token) external {
 		require (_amount > 0, "The amount must be greater than zero.");
 		bool result = stakingToken.transferFrom(msg.sender, address(this), _amount);
 		require (result, "Transfer from error");
-		balances[msg.sender] += _amount;
-		totalStake += _amount;
+
+		Staker memory staker;
+		staker.addrStaker = msg.sender;
+		staker.token = _token;
+		staker.amount = _amount;
+		staker.date = block.timestamp;	
+
+		totalStakes[_token].amount += _amount;
 	}
 
 	/**
 	 * @notice Withdraw fund into this contract
 	 * @param _amount to stake
 	 */
-	function withdraw (uint _amount) external {
+	function withdraw (uint256 _id, uint _amount, address _token) external {
 		require (_amount > 0, "The amount must be greater than zero.");
 		bool result = stakingToken.transfer(msg.sender, _amount);
 		require (result, "Transfer from error");
-		balances[msg.sender] -= _amount;
-		totalStake -= _amount;
+
+		delete stakers[_id];
+
+		totalStakes[_token].amount -= _amount;
 	}
 }
