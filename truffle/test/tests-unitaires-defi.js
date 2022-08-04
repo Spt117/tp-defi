ef4b92108ec48422bc0e25199f3e6f63c9bb63e3
Anthony DG <adegorlof@gmail.com>
2022-08-04 21:50:31 +0200

    Unit tests 05

--- truffle/test/tests-unitaires-defi.js
+++ truffle/test/tests-unitaires-defi.js
@@ -7,8 +7,9 @@ const StackingInstance = artifacts.require('Staking');
 contract("Staking", function (accounts) {
     const owner = accounts[0]; // Owner
     const stacker1 = accounts[1]; // Stacker
-    const ownerTokenTest = accounts[2]; // Celui qui a déployé TokenTest.sol
-    const otherAdress = accounts[3]; // adresse d'un inconnu.
+    const stacker2 = accounts[2]; // Stacker
+    const ownerTokenTest = accounts[3]; // Celui qui a déployé TokenTest.sol
+    const otherAdress = accounts[4]; // adresse d'un inconnu.
     let approvedAmount1 = 100000000;
     let transferredAmount1 = 10000000;
     let stakedAmount0 = 0;
@@ -20,46 +21,12 @@ contract("Staking", function (accounts) {
         Stacking,
         StackingAddress; // StackingAddress = adresse du contrat de staking.
 
-        // console.log("owner ==> ", owner)
-        // console.log("stacker1 ==> ", stacker1)
-        // console.log("TokenTestAddress ==> ", TokenTestAddress)
-        // console.log("StackingAddress ==> ", StackingAddress)
-        // console.log("approvedAmount1 ==> ", approvedAmount1)
-        // console.log("stakedAmount1 ==> ", stakedAmount1)
-        // console.log("stakedAPR1 ==> ", stakedAPR1)
-        // console.log("Chainlink1 ==> ", Chainlink1)
-
     describe('Light beforeEach', function () {
-        beforeEach(async function () {
-            Stacking = await StackingInstance.new({from: owner});
-        })
-
-        context("stopEmergency()", function() {
-            it('Test on stopEmergency() : only Owner', async function () {
-                await expectRevert(Stacking.stopEmergency({from: otherAdress}), "Ownable: caller is not the owner")
-            });
-
-            it('Test on stopEmergency() : Status enabled', async function () {
-                let receipt = await Stacking.statut.call();
-                await expect(new BN(receipt)).to.be.bignumber.equal(new BN(0));
-            });
-
-            it('Test on stopEmergency() : Status disabled', async function () {
-                await Stacking.stopEmergency({from: owner})
-                receipt = await Stacking.statut.call();
-                await expect(new BN(receipt)).to.be.bignumber.equal(new BN(1));
-            });
-        })
-    })
-
-    describe('Big beforeEach', function () {
-
         beforeEach(async function () {
             TokenTesting = await TokenTestInstance.new({from: ownerTokenTest});
             TokenTestAddress = TokenTesting.address;
 
             Stacking = await StackingInstance.new({from: owner});
-            StackingAddress = Stacking.address;
         })
 
         context("addPool()", function() {
@@ -97,14 +64,23 @@ contract("Staking", function (accounts) {
                 await expectEvent(receipt, "StopPool", {tokenAddress: TokenTestAddress, date: new BN(block['timestamp'])});
             });
         })
+    })
+
+    describe('Big beforeEach', function () {
+
+        beforeEach(async function () {
+            TokenTesting = await TokenTestInstance.new({from: ownerTokenTest});
+            TokenTestAddress = TokenTesting.address;
+
+            Stacking = await StackingInstance.new({from: owner});
+            StackingAddress = Stacking.address;
+
+            await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
+            await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
+            await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+        })
 
         context("Stake()", function() {
-            beforeEach(async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
-            })
-
             it('Test on stacker1 : already staker', async function () {
                 await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
                 await expectRevert(Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1}), "You already stake this pool")
@@ -126,15 +102,21 @@ contract("Staking", function (accounts) {
 
                 await expectEvent(receipt, "Stake", {sender: stacker1, tokenAddress: TokenTestAddress, amount: new BN(stakedAmount1), date: new BN(block['timestamp'])});
             });
+
+            it('Test on stacker2 : test event', async function () {
+                await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1});
+
+                await TokenTesting.transfer(stacker2, transferredAmount1, {from: ownerTokenTest});
+                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker2}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+                let receipt = await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker2});
+                let blockNum = await web3.eth.getBlockNumber()
+                let block = await web3.eth.getBlock(blockNum)
+
+                await expectEvent(receipt, "Stake", {sender: stacker2, tokenAddress: TokenTestAddress, amount: new BN(stakedAmount1), date: new BN(block['timestamp'])});
+            });
         })
 
         context("addStake()", function() {
-            beforeEach(async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
-            })
-
             it('Test on stacker1 : already staker', async function () {
                 // await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
                 await expectRevert(Stacking.addStake(stakedAmount1, TokenTestAddress, {from: otherAdress}), "You are not a staker")
@@ -154,19 +136,33 @@ contract("Staking", function (accounts) {
             });
 
             it('Test on stacker1 : test event', async function () {
-                let receipt = await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1});
+                await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1});
+                console.log("stakedAmount1 : ", stakedAmount1);
+                console.log("TokenTestAddress : ", TokenTestAddress);
+                // console.log("stakedAmount1 : ", stakedAmount1);
+                // let receipt = await Stacking.addStake(stakedAmount1, TokenTestAddress, {from: stacker1});
+                // let blockNum = await web3.eth.getBlockNumber()
+                // let block = await web3.eth.getBlock(blockNum)
+
+                // await expectEvent(receipt, "Stake", {sender: stacker1, tokenAddress: TokenTestAddress, amount: new BN(stakedAmount1), date: new BN(block['timestamp'])});
+            });
+
+            it('Test on stacker2 : test event', async function () {
+                await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1});
+                
+                await TokenTesting.transfer(stacker2, transferredAmount1, {from: ownerTokenTest});
+                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker2}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
+                await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker2});
+                let receipt = await Stacking.addStake(stakedAmount1, TokenTestAddress, {from: stacker2});
                 let blockNum = await web3.eth.getBlockNumber()
                 let block = await web3.eth.getBlock(blockNum)
 
-                await expectEvent(receipt, "Stake", {sender: stacker1, tokenAddress: TokenTestAddress, amount: new BN(stakedAmount1), date: new BN(block['timestamp'])});
+                await expectEvent(receipt, "Stake", {sender: stacker2, tokenAddress: TokenTestAddress, amount: new BN(stakedAmount1), date: new BN(block['timestamp'])});
             });
         })
 
         context("withdraw()", function() {
             beforeEach(async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
                 await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
             })
 
@@ -192,21 +188,8 @@ contract("Staking", function (accounts) {
             });
         })
 
-        // @todo : PAS BESOIN DU BeforeEach. Mettre cette fonction tout en haut ???
-        // context("getLatestPrice", function() {
-        //     it('Test on Chainlink : get last price of a pair', async function () {
-        //         console.log("StackingAddress ==> ", StackingAddress)
-        //         let lastPrice = await Stacking.getLatestPrice(Chainlink1, {from: StackingAddress});
-        //         console.log("lastPrice ==> ", lastPrice)
-        //         // expect(lastPrice.description).to.be.equal("proposalVoter1");
-        //     });
-        // })
-
         context("calculateReward()", function() {
             beforeEach(async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
                 await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
             })
 
@@ -222,9 +205,6 @@ contract("Staking", function (accounts) {
 
         context("claimRewards()", function() {
             it('Test on stacker1 : already staker', async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
                 await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
 
                 await expectRevert(Stacking.claimRewards(TokenTestAddress, {from: otherAdress}), "You are not a staker")
@@ -233,9 +213,6 @@ contract("Staking", function (accounts) {
 
         context("getStaking()", function() {
             it('Check amount of a stacked pool', async function () {
-                await TokenTesting.transfer(stacker1, transferredAmount1, {from: ownerTokenTest});
-                await Stacking.addPool(TokenTestAddress, stakedAPR1, Chainlink1, {from: owner});
-                await TokenTesting.approve(StackingAddress, approvedAmount1, {from: stacker1}); // l.136 dans https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol
                 await Stacking.stake(stakedAmount1, TokenTestAddress, {from: stacker1})
 
                 let receipt = await Stacking.getStaking(TokenTestAddress , {from: stacker1});
